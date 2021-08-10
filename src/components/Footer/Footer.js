430b4f3f6b0dd4d3ceaa58f9f6d5f4f9a3793344
Adam Dubey <adubey0311@gmail.com>
2021-08-10 11:50:54 -0400

    [AD][08M1021] Refactoring README, Header and Footer components

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Fade } from 'react-awesome-reveal';
 
 const Footer = () => {
     return (
-        <>
-        footer
-        </>
+        <footer className="bck_red">
+            <Fade triggerOnce>
+                <div className="font_righteous footer_logo_venue">Venue</div>
+                <div className="footer_copyright">Venue 2021 - all rights reserved</div>
+            </Fade>
+        </footer>
     )
 }
 
