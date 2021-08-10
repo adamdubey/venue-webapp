65b6d9634102a45cbb0143b7a1ee434ba0c2a5b2
Adam Dubey <adubey0311@gmail.com>
2021-08-10 13:01:41 -0400

    [AD][08M1021] Create Carousel component and refactoring tasks

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './resources/styles.css';
 import Header from './components/Header/Header';
+import Featured from './components/Featured/Featured';
 import Footer from './components/Footer/Footer'; 
 
 const App = () => {
   return (
     <div className="App">
       <Header />
-      <h1>Venue: Live Entertainment</h1>
+      <>
+        <Featured />
+      </>
       <div style={{backgroundColor: 'red', height: '900px'}}></div>
       <div style={{backgroundColor: 'yellow', height: '900px'}}></div>
       <div style={{backgroundColor: 'green', height: '900px'}}></div>
